b76d5df18067f67d5816e9109f52342655431528
Arthur <peraarthur2018@gmail.com>
2023-02-28 15:21:14 -0300

    styles + skeleton project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
 import { GlobalStyle } from "./styles/global";
-import { MainContainer } from "./styles/styles";
+import { BannerPizza, BannerScroll, DescriptionLaMassa, MainContainer } from "./styles/styles";
+import Pizza from "../src/assets/pizzaAbout.png"
 
 export function App() {
   
   return (
     <MainContainer>
+      <BannerPizza>
+        <h1>La Massa</h1>
+        <h2>PIZZARIA E RESTAURANTE</h2>
+      </BannerPizza>
+      <DescriptionLaMassa>
+      <div>
+      <p>
+      A palavra que define a La Massa <br />
+      é: <strong>OPORTUNIDADE</strong>.
+      </p>
+
+      <p>
+      A La Massa surge quando um dos
+      seus donos que trabalhava como
+      delivery vê uma banca de um senhor
+      com pizzas pré cozidas e pizzas
+      sendo entregues a todo vapor, viu
+      ali entao um oportunidade para
+      desenvolver seu empreendorismo.
+      </p>
+
+      <p>
+      Ao lembrar de seu amigo, hoje
+      seu socio na La Massa, bom co-
+      zinheiro, resolve convida-lo para
+      criar aLa Massa, com o foco de
+      da a oportunidade as pessoas
+      de desfrutem de boas pizzas e
+      massas no conforto de casa.
+      </p>
+      </div>
+
+      <img src={Pizza} alt="" />
+      </DescriptionLaMassa>
+
+      <BannerScroll>
+        <div>
+          LA MASSA
+        </div>
+      </BannerScroll>
 
       <GlobalStyle />
     </MainContainer>
--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,5 +1,57 @@
 import styled from "styled-components";
 
-export const MainContainer= styled.div`
+import Banner from "../assets/banner.jpg"
+
+export const MainContainer = styled.div`
     display: flex;
+    flex-direction: column;
+`
+
+export const BannerPizza = styled.div`
+    width: 100%;
+    height: 850px;
+    background-image: url(${Banner});
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    font-family: 'Nunito', sans-serif;
+`
+
+export const DescriptionLaMassa = styled.div`
+    display: flex;
+    width: 100%;
+    margin-top: 50px;
+    margin-left: 90px;
+    img {
+        left: 600px;
+        position: absolute;
+        width: 1000px;
+        height: 500px;
+    }
+    div {
+        width: 500px;
+        text-align: left;
+        p {
+        margin-bottom: 30px;
+        font-size: 30px;
+    }
+    }   
+`
+
+export const BannerScroll = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 900px;
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 300px;
+    }
 `
